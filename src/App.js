5a259e0b8cd041c83c6ae9592674b69158c73dbc
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-06 11:05:07 +0530

    task-6

--- src/App.js
+++ src/App.js
@@ -4,75 +4,45 @@ import { Routes,Route } from 'react-router-dom';
 import About from './Pages/About';
 import Navbar from './componets/navbar/Navbar';
 import { Button } from 'react-bootstrap';
-
-const productsArr = [
-
-  {
-  
-  title: 'Colors',
-  
-  price: 100,
-  
-  imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-  
-  },
-  
-  {
-  
-  title: 'Black and white Colors',
-  
-  price: 50,
-  
-  imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-  
-  },
-  
-  {
-  
-  title: 'Yellow and Black Colors',
-  
-  price: 70,
-  
-  imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-  
-  },
-  
-  {
-  
-  title: 'Blue Color',
-  
-  price: 100,
-  
-  imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-  
-  }
-  
-  ]
+import MovieList from "./components/Movies/MovieList";
+import React, { useState } from "react";
 function App() {
 
   
   return (
-    <>   
-   <Navbar />
-      <Routes>
-        <Route path='/about' element={ <About></About>}> </Route> 
-        {/* <Route path="/home" element={<Home />}></Route>
-        <Route path="/store" element={<Store />}></Route> */}
-       </Routes>
+   <div>
+    const [movies, setMovies] = useState([]);
+  const fetchMoviesHandler =( async () => {
 
-       <ul>
-       {productsArr.map((item)=>(
-          <li>
-            <h1>{item.title}</h1>
-            {item.price}
-            <img src={item.imageUrl}></img>
-            <Button>Add To Cart</Button>
-          </li>
-       ))}
-       </ul>
-    </>
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      const loadMovies = [];
+      for (let key in data) {
+        loadMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadMovies);
+  });
+
+   </div>
   );
- 
+ <Header/>
+  return (
+    <div>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      <MovieList movies={movies} />
+      {/* <Header/>
+<Footer/>
+</div>
+<Footer/> */}
+    </div>
+  );
 }
+
 
 export default App;
